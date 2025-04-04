7eba04f279dbe35da67828cb7642e81a5e7f2e12
Pabasara Samarakoon <pabasara96samarakoon@gmail.com>
2025-04-04 12:04:16 +0530

    Add files via upload

--- dip/nonlinearFilter.m
+++ dip/nonlinearFilter.m
@@ -1,5 +1,18 @@
-I = imread('./images/chessboard.png');
+I = imread('./images/saltPaper.png');
+if length(size(I))==3
+    I = rgb2gray(I);
+end
+%Minimum Filter
 f = @(x) min(x(:));
-J = nlfilter(I,[3 3],f);
-subplot(1,2,1);imshow(I);hold on;
-subplot(1,2,2);imshow(J);
+JMin = nlfilter(I,[3 3],f);
+%Maximum Filter
+f = @(x) max(x(:));
+JMax = nlfilter(I,[3 3],f);
+%Median Filter
+f = @(x) median(x(:));
+JMed = nlfilter(I,[3 3],f);
+
+subplot(1,4,1);imshow(I);hold on;
+subplot(1,4,2);imshow(JMin);hold on;
+subplot(1,4,3);imshow(JMax);hold on;
+subplot(1,4,4);imshow(JMed);
